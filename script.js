e630e68e24c6f78d1cb297a18e6eec34bad774c8
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2024-10-01 21:01:32 -0400

    updated functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //set up event listener
-
 document.getElementById('generationType').addEventListener('change', function() {
     const type = this.value;
     const additionalInputs = document.getElementById('additionalInput');
@@ -8,93 +7,234 @@ document.getElementById('generationType').addEventListener('change', function() 
     additionalInputs.innerHTML = '';
 
     // Show inputs based on selection
-    if (type === 'full' || type === 'partial') {
+    if (type === 'full' || type === 'partial' || type === 'story') {
         additionalInputs.innerHTML += `
-            <select id="gender">
+            <select class="dropdown form-select" id="gender">
                 <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
             </select>
-            <select id="nationality">
+            <select class="dropdown form-select" id="nationality">
                 <option value="">Select Nationality</option>
+                <option value="Random">Random</option>
                 <option value="American">American</option>
-                <option value="British">British</option>
+                <option value="French">French</option>
                 <option value="Chinese">Chinese</option>
                 <option value="Korean">Korean</option>
                 <option value="Vietnamese">Vietnamese</option>
                 <option value="Japanese">Japanese</option>
+                <option value="Thai">Thai</option>
+                <option value="Italian">Italian</option>
+                <option value="Spanish">Spanish</option>
+                <option value="African American">African American</option>
+                <option value="Indian">Indian</option>
+                <option value="Filipino">Filipino</option>
+                <option value="Greek">Greek</option>
+                <option value="Arabic">Arabic</option>
+                <option value="Nigerian">Nigerian</option>
+                <option value="Hawaiian">Hawaiian</option>
             </select>
         `;
-    } else if (type === 'randomNames') {
+    } else if (type === 'random') {
         additionalInputs.innerHTML += `
-            <select id="genderForNames">
-                <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
+            <select class="dropdown form-select" id="typeRandom">
+                <option value="">Select Type</option>
+                <option value="First">First Name</option>
+                <option value="Last">Last Name</option>
+                <option value="Both">First and Last Name</option>
+                <option value="Pet">Pet Name</option>
             </select>
-            <select id="nationalityForNames">
+
+            <input class="form-control" type="text" id="customName" name="customName" autocomplete="off" placeholder="Enter your custom First or Last Name">
+
+            <select class="dropdown form-select" id="genderRandom">
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </select>
+            <select class="dropdown form-select" id="nationalityRandom">
                 <option value="">Select Nationality</option>
+                <option value="Random">Random</option>
                 <option value="American">American</option>
-                <option value="British">British</option>
-                <option value="Canadian">Canadian</option>
+                <option value="French">French</option>
+                <option value="Chinese">Chinese</option>
+                <option value="Korean">Korean</option>
+                <option value="Vietnamese">Vietnamese</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Thai">Thai</option>
+                <option value="Italian">Italian</option>
+                <option value="Spanish">Spanish</option>
+                <option value="African American">African American</option>
+                <option value="Indian">Indian</option>
+                <option value="Filipino">Filipino</option>
+                <option value="Greek">Greek</option>
+                <option value="Arabic">Arabic</option>
+                <option value="Nigerian">Nigerian</option>
+                <option value="Hawaiian">Hawaiian</option>
             </select>
             <input type="range" id="numberOfNames" min="1" max="20" value="10" oninput="this.nextElementSibling.value = this.value">
             <output>10</output>
         `;
-    } else if (type === 'nameMatching') {
-        additionalInputs.innerHTML += `
-            <input type="text" id="lastName" placeholder="Last Name">
-            <input type="number" id="numberOfMatches" placeholder="Number of Matches (e.g., 10)">
-        `;
-    }
+    } 
 });
 
-document.getElementById('generateButton').addEventListener('click', function() {
-    const type = document.getElementById('generationType').value;
-    const nationality = document.getElementById('nationality')?.value || document.getElementById('nationalityForNames')?.value;
-    const gender = document.getElementById('gender')?.value || document.getElementById('genderForNames')?.value;
-    const numberOfNames = document.getElementById('numberOfNames').value;
-    const lastName = document.getElementById('lastName')?.value;
-    const numberOfMatches = document.getElementById('numberOfMatches')?.value;
+document.getElementById('generate').addEventListener('click', function() {
+    const genType = document.getElementById('generationType').value;
+    const nationality = document.getElementById('nationality')?.value || document.getElementById('nationalityRandom')?.value;
+    const gender = document.getElementById('gender')?.value || document.getElementById('genderRandom')?.value;
+    
+    const generatedOutput = document.getElementById('output');
+
+    document.getElementById('output').innerHTML = ''; // Clear previous outputs
+
+    // functions & their variables
+
+    function getRandomAgeCategory(){
+        const ageCategories = ['Child', 'Teen', 'Young Adult', 'Adult', 'Elder'];
+        const randomIndex = Math.floor(Math.random() * ageCategories.length);
+        return ageCategories[randomIndex];
+    };
+
+    const randomAge = getRandomAgeCategory();
+
+    function getDetails(details){
+        return new Promise((resolve, reject) => { // return a promise
+            let file = 'details.csv';
+            const detail = details;
+            Papa.parse(file, {
+                header: true,
+                download: true,
+                complete: function(results) {
+                    if (detail === 'job') { 
+                        const data = results.data;
+                        const occupationsFilteredData = data.filter(item => item.Type === 'Job');
+    
+                        let job = '';
+    
+                        // Select a random job if available
+                        if (occupationsFilteredData.length > 0) {
+                            job = occupationsFilteredData[Math.floor(Math.random() * occupationsFilteredData.length)].Title;
+                        } else {
+                            job = 'No matching jobs found.';
+                        }
+                        
+                        resolve(job); // Resolve the promise with the job
+                    } else {
+                        reject('No matching detail'); // Reject the promise if the detail doesn't match
+                    }
+                }
+            });
+        });
+    }   
+
 
     // Load the CSV file and generate names based on selections
-    Papa.parse('names.csv', {
-        download: true,
+    
+    let file = 'names.csv'
+    Papa.parse(file, {
         header: true,
+        download:true,
         complete: function(results) {
             const data = results.data;
-            let output = '';
+            // let output = '';
 
-            if (type === 'fullProfile' || type === 'partialProfile') {
-                // Generate a full or partial profile
-                const filteredData = data.filter(item => item.Nationality === nationality && item.Gender === gender);
-                if (filteredData.length > 0) {
-                    const randomItem = filteredData[Math.floor(Math.random() * filteredData.length)];
-                    output = `Generated Profile: ${randomItem.Name}`;
+            if (genType === 'full' || genType === 'partial' || genType ==='story') {
+                // Generate a full, partial, or story
+
+               // Generating the names
+
+
+                const firstFilteredData = data.filter(item => item.Nationality === nationality && item.Gender === gender && item.Type === 'First');
+                const lastFilteredData = data.filter(item => item.Nationality === nationality && item.Type === 'Last');
+
+                let firstName = '';
+                let lastName = '';
+
+                // Select a random first name if available
+                if (firstFilteredData.length > 0) {
+                    firstName = firstFilteredData[Math.floor(Math.random() * firstFilteredData.length)].Name;
                 } else {
-                    output = 'No matching profiles found.';
+                    firstName = 'No matching first names found.';
                 }
-            } else if (type === 'randomNames') {
-                // Generate random names
-                const filteredData = data.filter(item => item.Nationality === nationality && item.Gender === gender);
-                const names = [];
-                for (let i = 0; i < numberOfNames; i++) {
-                    const randomItem = filteredData[Math.floor(Math.random() * filteredData.length)];
-                    if (randomItem) names.push(randomItem.Name);
-                }
-                output = `Random Names: ${names.join(', ')}`;
-            } else if (type === 'nameMatching') {
-                // Generate names matching the last name
-                const filteredData = data.filter(item => item.Nationality === nationality && item.Name.endsWith(lastName));
-                const names = [];
-                for (let i = 0; i < numberOfMatches; i++) {
-                    const randomItem = filteredData[Math.floor(Math.random() * filteredData.length)];
-                    if (randomItem) names.push(randomItem.Name);
-                }
-                output = `Matching Names: ${names.join(', ')}`;
-            }
 
-            document.getElementById('output').innerText = output;
+                // Select a random last name if available
+                if (lastFilteredData.length > 0) {
+                    lastName = lastFilteredData[Math.floor(Math.random() * lastFilteredData.length)].Name;
+                } else {
+                    lastName = 'No matching last names found.';
+                }
+
+                // Combine first and last name for output
+                names = `${firstName} ${lastName}`;
+
+                if(genType == 'full'){
+                    getDetails('job').then(job => {
+                        generatedOutput.innerHTML += `
+                            <p class="outputText">Generated Profile:</p>
+                            <p class="outputText">Nationality: ${nationality}</p>
+                            <p class="outputText">Name: ${names}</p>
+                            <p class="outputText">Age: ${randomAge}</p>
+                            <p class="outputText">Job: ${job}</p>
+                        `;
+                    }).catch(error => {
+                        console.error(error);
+                    });
+
+                } else if(genType == 'partial'){
+                    generatedOutput.innerHTML += `
+                            <p class="outputText">Generated Profile:</p>
+                            <p class="outputText">Nationality: ${nationality}</p>
+                            <p class="outputText">Name: ${names}</p>
+                            <p class="outputText">Age: ${randomAge}</p>
+
+                    `;
+                } else if(genType == 'story'){
+                    generatedOutput.innerHTML += `
+                            <p class="outputText">Generated Profile:</p>
+                            <p class="outputText">Nationality: ${nationality}</p>
+                            <p class="outputText">Name: ${names}</p>
+                            <p class="outputText">Age: ${randomAge}</p>
+
+                    `;
+
+                }
+
+
+            } else if (genType === 'random') { 
+                // Generate random names
+                const numberOfNames = document.getElementById('numberOfNames').value;
+                const randomType = document.getElementById('typeRandom').value;
+                const customName = document.getElementById('customName').value;
+
+                console.log('random generation')
+                console.log(`number of names is ${numberOfNames}`)
+                console.log(`custom name is${customName} `)
+
+                if(randomType == 'First'){
+                    const firstFilteredData = data.filter(item => item.Nationality === nationality && item.Gender === gender && item.Type === 'First');
+                    for (let i = 0; i < numberOfNames; i++) {
+                        const randomFirst = firstFilteredData[Math.floor(Math.random() * firstFilteredData.length)].Name;
+                        names = `${randomFirst} ${customName}`;
+                        generatedOutput.innerHTML += `
+                            <p class="outputText">Name: ${names}</p>
+                    `;
+                    }                             
+                }else if(randomType == 'Last'){
+                    const lastFilteredData = data.filter(item => item.Nationality === nationality && item.Type === 'Last');
+                    for (let i = 0; i < numberOfNames; i++) {
+                        const randomLast = lastFilteredData[Math.floor(Math.random() * lastFilteredData.length)].Name;
+                        names = `${customName} ${randomLast} `;
+                        generatedOutput.innerHTML += `
+                            <p class="outputText">Name: ${names}</p>
+                    `;
+                    }        
+
+                }else if(randomType == 'Both'){
+
+                }
+
+
+            } 
         }
     });
 });
